4f90f13b3be00abcf654700cbee5528e9003e993
nahom <nahomdebele002@gmail.com>
2024-02-24 23:50:41 +0300

    added the clean code artecture with bloc state managment

--- lib/features/news/data/repositories/article_repository_impl.dart
+++ lib/features/news/data/repositories/article_repository_impl.dart
@@ -1,7 +1,10 @@
 import 'package:extraedmobile/core/resources/data_state.dart';
-import 'package:extraedmobile/features/news/domain/enties/article.dart';
+import 'package:extraedmobile/features/news/data/models/article.model.dart';
 import 'package:extraedmobile/features/news/domain/repositories/article_reposotory.dart';
 
-abstract class ArticleRepositoryImpl  implements ArticleRepository {
-  Future<DataState<List<ArticleEntity>>> getNewsArticles();
+class ArticleRepositoryImpl  implements ArticleRepository {
+  @override
+  Future<DataState<List<ArticelModel>>> getNewsArticles() {
+    throw UnimplementedError();
+  }
 }
