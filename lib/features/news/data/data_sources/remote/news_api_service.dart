a62fc8405a908df30a15a5c58cd9c6f70f5a4e76
nahom <nahomdebele002@gmail.com>
2024-02-24 23:51:15 +0300

    added the clean code artecture with bloc state managment

---
+++ lib/features/news/data/data_sources/remote/news_api_service.dart
@@ -0,0 +1,17 @@
+
+// import 'dart:io';
+
+// import 'package:dio/dio.dart';
+// import 'package:extraedmobile/config/constants/constants.dart';
+// import 'package:extraedmobile/features/news/data/models/article.model.dart';
+
+// part 'news_api_service.g.dart';
+
+// @RestApi(baseUrl: baseUrl )
+// abstract class NewsApiService {
+//   factory NewsApiService(Dio dio) = _NewsApiService;
+//   @Get('/top-headlines')
+//   Future<HttpResponse<List<ArticelModel>>> getNewsArticles() {
+
+//   }
+// }
